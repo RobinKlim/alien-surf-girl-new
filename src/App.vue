29472f79b476bb519061712170028bce7936fbd8
Robin <klimczak.robin@gmail.com>
2022-10-24 11:11:16 +0200

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,57 @@
 <template>
-  <nav>
+    <nav class="navigation-home-container">
+      <img src="@/assets/CD-ROM.png" class="navigation-home-img" alt="">
+      <ul class="navigation-home-list">
+        <li><a href="#">Hits</a></li>
+        <li><a href="#">Shows</a></li>
+        <li><a href="#">Über uns</a></li>
+        <li><a href="#">Kontakt</a></li>
+      </ul>
+    </nav>
 <!--
 <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
 -->
-  </nav>
   <router-view/>
 </template>
 
-<style>
+<style scoped>
+
+/* Navigation Design*/
+.navigation-home-container {
+  position: fixed;
+  width: 70%;
+  left: 50%;
+  top: 50%;
+  translate: -50% -50%;
+}
+.navigation-home-img{
+  width: 100%;
+}
+.navigation-home-list{
+  width: 100%;
+  height: 100%;
+}
+
+li {
+  position: absolute;
+  display: inline;
+  transform-origin: left;
+  left: 50%;
+  top: 50%;
+  translate: 0 -50% ;
+  padding-left: 22%;
+}
+
+li:nth-child(2) {
+  rotate: 90deg;
+}
+li:nth-child(3) {
+  rotate: 180deg;
+}
+li:nth-child(4) {
+  rotate: 270deg;
+}
+
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="home">
-    <div @click="opacityZero()" class="cd-rom-container-all">
-      <CDCover class="cd-cover"/>
-      <div class="navigation-home-container">
-        <img src="@/assets/CD-ROM.png" class="navigation-home-img" alt="">
-        <nav class="navigation-home">
-          <ul>
-            <li><a href="#">Ey Sarah,</a></li>
-            <li><a href="#">mein Y2K -Girl</a></li>
-            <li><a href="#">wir haben</a></li>
-            <li><a href="#">Samstag</a></li>
-            <li><a href="#">ein Date.</a></li>
-            <li><a href="#">Kennst du</a></li>
-            <li><a href="#">Würfel & Zucker?</a></li>
-            <li><a href="#">( ͡° ͜ʖ ͡°) </a></li>
-          </ul>
-        </nav>    
-      </div>
-    </div>
+    <CDCover v-if="cdClicked === false" @click="opacityZero()" class="cd-cover"/>
   </div>
 </template>
 
@@ -26,15 +9,19 @@ import CDCover from '../components/CDCover'
 
 export default {
   name: 'HomeView',
-  components: { CDCover,},
+  components: { CDCover},
   data() {
     return {
-      CDNotClicked: true
+      cdClicked: false
     }
   },
   methods: {
     opacityZero() {
       document.querySelector(".cd-cover").style.opacity = "0"
+      setTimeout(this.removeCoverDom, 1000)
+    },
+    removeCoverDom() {
+      this.cdClicked = true;
     }
   }
 }
@@ -53,15 +40,8 @@ export default {
   background-size: cover;
   background-position: center center;
 }
-.cd-rom-container-all {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  width: 90%;
-}
-
 .cd-cover {
+  width: 80%;
   animation: pulse 2s ease-in-out infinite;
   transition: 1s;
   z-index: 10;
@@ -78,59 +58,6 @@ export default {
   }
 }
 
-/* Navigation Design*/
-.navigation-home-container {
-  position: absolute;
-  width: 85%;
-  left: 13%;
-  top: 1%;
-}
-.navigation-home-img{
-  width: 100%;
-}
-.navigation-home{
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 100%;
-}
-ul {
-  height: 100%;
-  position: relative;
-  display: flex;
-  align-items: center;
-}
-li {
-  position: absolute;
-  transform-origin: left;
-  display: inline;
-  left: 50%;
-  padding-left: 23%;
-}
-li:nth-child(1) {
-  rotate: 45deg;
-}
-li:nth-child(2) {
-  rotate: 90deg;
-}
-li:nth-child(3) {
-  rotate: 135deg;
-}
-li:nth-child(4) {
-  rotate: 180deg;
-}
-li:nth-child(5) {
-  rotate: 225deg;
-}
-li:nth-child(6) {
-  rotate: 270deg;
-}
-li:nth-child(7) {
-  rotate: 315deg;
-}
-li:nth-child(7) {
-  rotate: 315deg;
-}
 
 
 </style>
