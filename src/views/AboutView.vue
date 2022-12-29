88817bd91f48e3b8e082427e28bb4ced33d39dd5
rklimczak <rklimczak@novomind.com>
2022-12-29 17:33:52 +0100

    cardRoll

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,11 +6,11 @@
       <p class="newsText">+++ Alien Surf Girl bringt verspielt Nu-Disco Pop in das Gewand einer 2000er Boyband. Weiß-lackierte Fingernägel, die Eleganz von Feinripp und Silber-Schmuck, kombiniert mit einer weichen und zärtlichen Art. +++</p>
     </div>
     <div class="card-deck">
-      <img id="cardJonas" src="@/assets/SingleShots_Cards/Spielkarten_Jonas.png">
-      <img id="cardCyrus" src="../assets/SingleShots_Cards/Spielkarten_Cyrus.png" @click="moveImage">
-      <img id="cardPhilipp" src="@/assets/SingleShots_Cards/Spielkarten_Philipp.png">
-      <img id="cardMorten" src="@/assets/SingleShots_Cards/Spielkarten_Morten.png">
-      <img id="cardRobin" src="@/assets/SingleShots_Cards/Spielkarten_Robin.png">
+      <img class="single-card" id="cardJonas" src="@/assets/SingleShots_Cards/Spielkarten_Jonas.png" @click="moveImage">
+      <img class="single-card" id="cardCyrus" src="../assets/SingleShots_Cards/Spielkarten_Cyrus.png" @click="moveImage">
+      <img class="single-card" id="cardPhilipp" src="@/assets/SingleShots_Cards/Spielkarten_Philipp.png" @click="moveImage">
+      <img class="single-card" id="cardMorten" src="@/assets/SingleShots_Cards/Spielkarten_Morten.png" @click="moveImage">
+      <img class="single-card" id="cardRobin" src="@/assets/SingleShots_Cards/Spielkarten_Robin.png" @click="moveImage">
     </div>
   </div>
 </template>
@@ -36,16 +36,39 @@ import Overlay from '@/components/Overlay'
       this.windowWidth = document.querySelector('html').clientWidth;
       document.documentElement.style.setProperty('--windowWidth', `${this.windowWidth}px`)
       // add eventlistener to Top card
-      if(5 == 5) {
-        console.log("sheesh")
-      }
     },
     methods: {
-      moveImage() {
-        console.log("test")
-        const value = document.getElementById("cardCyrus").style.getPropertyValue("z-index")
-        console.log(value)
+      moveImage(e) {
+        const el = e.target  // clicked image
+        const zIndexTop = getComputedStyle(el).getPropertyValue("z-index")
+        console.log(e)
 
+        // check if selected card is on top
+        if(zIndexTop == 5) {
+          //change order by changing zIndices
+          const allSingleCardsArr = document.getElementsByClassName("single-card");
+
+          for(let i = 0; i < allSingleCardsArr.length; i++)
+          {
+            const zIndex = getComputedStyle(allSingleCardsArr[i]).getPropertyValue("z-index")
+            if(zIndex == 1) {
+              allSingleCardsArr[i].style.zIndex = 2
+            }
+            if(zIndex == 2) {
+              allSingleCardsArr[i].style.zIndex = 3
+            }
+            if(zIndex == 3) {
+              allSingleCardsArr[i].style.zIndex = 4
+            }
+            if(zIndex == 4) {
+              allSingleCardsArr[i].style.zIndex = 5
+            }
+            if(zIndex == 5) {
+              allSingleCardsArr[i].style.zIndex = 1
+            }
+          }
+          // el.style.zIndex = "0"
+        }
       }
     }
   }
